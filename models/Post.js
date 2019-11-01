8e4f4977bf4630de87a2d466051bf1488191ea55
Juliana Almeida <julianaferreira@gmail.com>
2019-11-01 11:13:48 -0700

    Modified Mongoose Schema

--- models/Post.js
+++ models/Post.js
@@ -3,20 +3,25 @@ const mongoose = require('mongoose');
 const PostSchema = new mongoose.Schema({
   title: {
     type: String
-    
   },
   image_url: {
     type: String
-    
   },
   date_posted: {
     type: Date
-    
   },
   location: {
     type: String
-    
-  }
+  },
+  latitude: {
+    type: Number
+  },
+  longitude: {
+    type: Number
+  },
+  claimed: {
+    type: Boolean
+  },
 });
 
 module.exports = Post = mongoose.model('Post', PostSchema);
