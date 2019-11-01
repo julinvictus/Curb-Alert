1a55e2042cc5bd9ae68bd4f3c9ecdb0001a458e3
Juliana Almeida <julianaferreira@gmail.com>
2019-11-01 15:53:34 -0700

    Added bodyParser middleware

--- app.js
+++ app.js
@@ -2,18 +2,27 @@ const express = require('express');
 const connectDB = require('./config/db');
 require('dotenv').config();
 const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
 const usersRoutes = require('./routes/api/users');
 const postRoutes = require('./routes/api/posts');
 const sign_s3 = require('./controllers/sign_s3');
-
-const cors = require('cors');
-const port = process.env.PORT || 5000;
 
 // Connect Database
 connectDB()
 
 // cors
 app.use(cors({ origin: true, credentials: true }));
+
+// body-parser
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
 
 app.get('/', (request, response) => {
     response.json({ info: 'Node.js, Express, and MongoDB API' })
